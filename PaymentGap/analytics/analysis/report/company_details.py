aa2e63e1d1f2de9e119b1c596693fe3391f87337
GabrielaSpiridon <gabriela.spiridon2003@yahoo.com>
2025-07-13 13:48:54 +0300

    Quick update

--- PaymentGap/analytics/analysis/report/company_details.py
+++ PaymentGap/analytics/analysis/report/company_details.py
@@ -2,10 +2,10 @@ from ..models.company_entity import CompanyEntity
 
 def get_company_details():
     # 'select_related' gives us the related objects in a single query
-    ce = CompanyEntity.objects.select_related('id_region', 'id_country').first()
+    ce = CompanyEntity.objects.select_related('id_company','id_region', 'id_country').first()
 
     return {
-        "company_name": ce.company_entity_name,
+        "company_name": ce.id_company.company_name,
         "region":       ce.id_region.region_name,  
         "country":      ce.id_country.country_name, 
 
